acd6d665361b38b38dcb1a513ca83a09f741603a
alimbadmus <alimbadmus239@gmail.com>
2026-07-28 21:43:12 +0100

    added auto updater

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -33,11 +33,14 @@ pub struct AppState {
 // App entry point
 // ---------------------------------------------------------------------------
 
+    
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .setup(|app| {
             let app_handle = app.handle().clone();
 
